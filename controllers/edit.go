25ff1e68120d8d9bec8e91757003c7c353d5b0fa
bugong <1761869346@qq.com>
2020-06-12 07:19:04 +0800

    edit: model tag

--- controllers/edit.go
+++ controllers/edit.go
@@ -3,11 +3,22 @@ package controllers
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	. "prot/models"
 )
 
 func RenderEdit(c *gin.Context) {
-	c.HTML(http.StatusOK, "edit", gin.H{})
+	tag := Tag{}
+	tags, err := tag.Tags()
+
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "error", gin.H{
+			"statusCode": http.StatusInternalServerError,
+			"message": err,
+		})
+		return
+	}
+
+	c.HTML(http.StatusOK, "edit", gin.H{
+		"tags": tags,
+	})
 }
-
-
-
