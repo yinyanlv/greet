c247e73a5cf7c5ca6c143bc59d001147a9af06e9
bugong <1761869346@qq.com>
2020-06-15 06:52:54 +0800

    edit: tag model

--- models/tag.go
+++ models/tag.go
@@ -32,10 +32,30 @@ func (tag Tag) Insert() (id uint, err error) {
 	return
 }
 
-func (tag *Tag) Update() () {
+func (tag *Tag) Update(id uint) (updatedTag TagResult, err error) {
+	if err = MysqlDB.Select([]string{"code", "name"}).First(&updatedTag, id).Error; err != nil {
+		return
+	}
 
+	if err = MysqlDB.Model(&updatedTag).Updates(tag).Error; err != nil {
+		return
+	}
+
+	return
 }
 
-func (tag *Tag) Delete() () {
+func (tag *Tag) Delete(id uint) (deleledTag TagResult, err error) {
 
+	if err = MysqlDB.Select([]string {"code", "name"}).First(tag, id).Error; err != nil {
+		return
+	}
+
+	if err = MysqlDB.Delete(tag).Error; err != nil {
+		return
+	}
+
+	deleledTag.Code = (*tag).Code
+	deleledTag.Name = (*tag).Name
+
+	return
 }
